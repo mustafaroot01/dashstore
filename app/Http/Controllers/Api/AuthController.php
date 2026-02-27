1141932712c97461a284a187662be96143228cf2
MRootxx <dr.rootvb@gmail.com>
2026-02-27 20:27:07 +0300

    Fix AuthController Setting import error

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Setting;
 use App\Services\OtpService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
